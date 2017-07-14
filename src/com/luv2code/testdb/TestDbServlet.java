578b6daa984a09c530661d6b349abcd478ee37bb
J.Wang <johnnywang7119@gmail.com>
2017-07-13 23:57:55 -0400

    first commit

---
+++ src/com/luv2code/testdb/TestDbServlet.java
@@ -0,0 +1,50 @@
+package com.luv2code.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//setup connection variables
+		String user = "sprintstudent";
+		String pass = "sprintstudent";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		String dirver = "com.mysql.jdbc.Driver";
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to databse: " + jdbcUrl);
+			Connection myConn= DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			out.println("Success!");
+			myConn.close();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+	}
+
+}
